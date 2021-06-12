f6820e81f005eeaf3229032b9ceea07a01809793
jayeshdesai4520 <jayeshdesai4520@gmail.com>
2021-06-13 01:29:03 +0530

    Added sample weather api function

--- Webhook/app.js
+++ Webhook/app.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const express = require('express')
 const {WebhookClient} = require('dialogflow-fulfillment')
 const app = express()
@@ -14,14 +15,34 @@ app.post('/webhook', (req, res) => {
     let agent = new WebhookClient({request: req, response: res})
     // create intentMap for handle intent
     let intentMap = new Map();
-    // add intent map 2nd parameter pass function
-    intentMap.set('webhook_test_intent',handleWebHookIntent)
+    // add intent map 2nd parameter pass function 
+    intentMap.set('weather',weatherIntent)
     // now agent is handle request and pass intent map
     agent.handleRequest(intentMap)
 })
-function handleWebHookIntent(agent){
-    agent.add("Hello I am Webhook demo How are you...")
+
+ 
+ 
+function weatherIntent(agent){ 
+    const apiKey = "23756992e06787aa9225e9b361dfcd66"
+    const city = agent.parameters.city; 
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+    return axios.get(url)
+      .then(function (response) {
+        // handle success
+        console.log(response.data.main.temp); 
+        agent.add("Temp in " + city +  " is " + response.data.main.temp)
+        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
 }
+
 /**
 * now listing the server on port number 3000 :)
 * */
