43803d7e7e96c1448368fca01cd236d1cbe639a4
jayeshdesai4520 <jayeshdesai4520@gmail.com>
2021-06-22 14:53:38 +0530

    fix code

--- Webhook/app.js
+++ Webhook/app.js
@@ -6,54 +6,34 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send("ok Server Is Working......")
 })
+
+var kbquestion
+
 /**
 * on this route dialogflow send the webhook request
 * For the dialogflow we need POST Route.
 * */
-app.post('/webhook', (req, res) => {
+app.post('/webhook', (request, response) => {
     // get agent from request
-    let agent = new WebhookClient({request: req, response: res})
+    let agent = new WebhookClient({request: request, response: response})
     // create intentMap for handle intent
     let intentMap = new Map();
+    kbquestion = request.body.queryResult.queryText
     // add intent map 2nd parameter pass function
     intentMap.set('webhook_test_intent',weatherIntent)
-    intentMap.set('show_component_list',active_components_Intent)
-    intentMap.set('reset_list_of_components',reset_components_list_Intent)
-    intentMap.set('Deactivate_component_intent',Deactivate_component_intent)
-    intentMap.set('Activate_component_intent',Activate_component_intent)
+    intentMap.set('Show component list Intent',Active_components_Intent)
+    intentMap.set('Reset list of components Intent',Reset_components_list_Intent)
+    intentMap.set('Deactivate component Intent',Deactivate_component_Intent)
+    intentMap.set('Activate component Intent',Activate_component_Intent)
+    intentMap.set('Default Fallback Intent', fallback);
     // now agent is handle request and pass intent map
     agent.handleRequest(intentMap)
 })
 
-var activecomponents = [];
+var activecomponents = [NED-DBpediaSpotlight,QueryBuilderSimpleRealNameOfSuperHero,SparqlExecuter,OpenTapiocaNED,BirthDataQueryBuilder,WikidataQueryExecuter];
+  
 
-
-
-// just for API test
-function weatherIntent(agent){ 
-    const apiKey = "23756992e06787aa9225e9b361dfcd66"
-    const city = agent.parameters.city; 
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
-    return axios.get(url)
-      .then(function (response) {
-        // handle success
-        console.log(response.data); 
-        agent.add("Temp in " + city +  " is " + response.data.main.temp)
-        
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-}
-
-
-//  
-
-function active_components_Intent(agent){ 
+function Active_components_Intent(agent){ 
     if (activecomponents.length === 0){
         agent.add("currently, there are no active components")     
     } else {
@@ -62,25 +42,39 @@ function active_components_Intent(agent){
 }
 
 
-function reset_components_list_Intent(agent){ 
+function Reset_components_list_Intent(agent){ 
         activecomponents = []
         agent.add("Components list are now empty")  
         console.log(activecomponents)   
 }
 
 
-function Deactivate_component_intent(agent){ 
+function Deactivate_component_Intent(agent){ 
         const deactivate = agent.parameters.componentname; 
         activecomponents.pop(deactivate); 
         agent.add("succesfully removed " + deactivate + " from components list")        
 }
 
 
-function Activate_component_intent(agent){ 
+function Activate_component_Intent(agent){ 
         //agent.add("To add the components in order you can say Add and then name of the components")
         const activate = agent.parameters.activatecomponent; 
         activecomponents.push(activate); 
         agent.add("succesfully Added " + activate + " you can add more components by saying Add and then name of the component")       
+}
+
+var query  = 'What is the real name of Batman?'
+function fallback(agent){          
+        agent.add(kbquestion) 
+        axios.post('https://webengineering.ins.hs-anhalt.de:43740/gerbil-execute/NED-DBpediaSpotlight,QueryBuilderSimpleRealNameOfSuperHero,SparqlExecuter,OpenTapiocaNED,BirthDataQueryBuilder,WikidataQueryExecuter', {
+            query
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });            
 }
 
 
