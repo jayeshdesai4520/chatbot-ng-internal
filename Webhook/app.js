e82820fbd67a96f32e49ad3e6b0922d34bc08111
jayeshdesai4520 <jayeshdesai4520@gmail.com>
2021-07-02 17:05:51 +0530

    add user session code

--- Webhook/app.js
+++ Webhook/app.js
@@ -8,10 +8,17 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send("ok Server Is Working......")
 })
+
 var session 
 var kbquestion
 var body 
 var defaultcomponents = ["NED-DBpediaSpotlight", "QueryBuilderSimpleRealNameOfSuperHero", "SparqlExecuter", "OpenTapiocaNED", "BirthDataQueryBuilder", "WikidataQueryExecuter"];
+var NED_Default = ["NED-DBpediaSpotlight"]
+var NER_Default = ["NER-DBpediaSpotlight"]
+var Querybuilder_Default = ["QueryBuilder"]
+var Queryexecuter_Default = ["SparqlExecuter"]
+
+
 let sessionidmanagement = new Map()
 /**
  * on this route dialogflow send the webhook request
@@ -35,61 +42,74 @@ app.post('/webhook', (request, response) => {
     intentMap.set('Deactivate component Intent', Deactivate_component_Intent)
     intentMap.set('Activate component Intent', Activate_component_Intent)
     intentMap.set('Default Fallback Intent', fallback);
-    // now agent is handle request and pass intent map
+    //now agent is handle request and pass intent map
     agent.handleRequest(intentMap)
 })
+
+
+app.get('/question', (req, res) => {
+    res.send(kbquestion)
+})
+
+
 
 const welcome_message_data = ['Hi! I am the DBpedia bot, How are you doing?','Hello! I am the DBpedia bot,  How can I help you?','Greetings! I am the DBpedia bot,  How can I assist?','Good day! I am the DBpedia bot,  What can I do for you today?']
 
 function Welcome_Intent(agent) {
-         
+        if(!sessionidmanagement.has(session)){
+        sessionidmanagement.set(session,defaultcomponents)
+        }
         const welcomeMessageArr = welcome_message_data;
         const textindex = Math.floor(Math.random() * welcomeMessageArr.length);
         const out = welcomeMessageArr[textindex];
         const speechOutput =  out; 
         agent.add(speechOutput)
-        console.log(session)
+        console.log(sessionidmanagement) 
 }
 
 
 //name saved components
 function Active_components_Intent(agent) {
-    if (activecomponents.length === 0) {
+    if (!sessionidmanagement.has(session)) {
         agent.add("currently, there are no active components")
     } else {
-        agent.add("currently, active components are " + activecomponents)
-    }
-    console.log(body)
+        agent.add("currently, active components are " + sessionidmanagement.get(session))
+    } 
+    console.log(sessionidmanagement)
 }
 
 
 function Reset_components_list_Intent(agent) {
-    activecomponents = []
-    agent.add("Components list are now empty")
-    console.log(activecomponents)
+    sessionidmanagement.clear()
+    agent.add("Components list are now empty") 
+    console.log(sessionidmanagement)
 }
 
 
 function Deactivate_component_Intent(agent) {
-    const deactivate = agent.parameters.componentname;
-    activecomponents.pop(deactivate);
+    const deactivate = agent.parameters.componentname; 
+    deletecomponent = sessionidmanagement.get(session)
+    deletecomponent = deletecomponent.replace("," + deactivate, "")
+    sessionidmanagement.set(session,deletecomponent) 
     agent.add("succesfully removed " + deactivate + " from components list")
+    console.log(sessionidmanagement)
 }
 
 
 function Activate_component_Intent(agent) {
     //agent.add("To add the components in order you can say Add and then name of the components")
-    const activate = agent.parameters.activatecomponent;
-    activecomponents.push(activate);
+    var activate = agent.parameters.activatecomponent; 
+    sessionidmanagement.set(session, sessionidmanagement.get(session) + "," +  activate)
     agent.add("succesfully Added " + activate + " you can add more components by saying Add and then name of the component")
+    console.log(sessionidmanagement)
 }
 
-//const query = 'What is the real name of spiderman?'
+
 
 function fallback(agent) {
 
 
-    return axios.post('https://webengineering.ins.hs-anhalt.de:43740/gerbil-execute/NED-DBpediaSpotlight,QueryBuilderSimpleRealNameOfSuperHero,SparqlExecuter,OpenTapiocaNED,BirthDataQueryBuilder,WikidataQueryExecuter?query=' + kbquestion, {
+    return axios.post('https://webengineering.ins.hs-anhalt.de:43740/gerbil-execute/' + sessionidmanagement.get(session)  + '?query=' + kbquestion, {
             headers: {
                 'content-type': 'text/plain'
             }
@@ -115,7 +135,7 @@ function fallback(agent) {
             agent.add(output) 
 
         }).catch(function(error) {
-            //console.log(error)
+            console.log(error)
             agent.add("No answer could be loaded. Try again? or you can ask for help!")
         });
 }
