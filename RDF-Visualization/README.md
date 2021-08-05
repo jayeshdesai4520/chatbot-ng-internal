fad13255bdf6230935c2e900b8fecd11335f54e4
jayeshdesai4520 <jayeshdesai4520@gmail.com>
2021-08-05 21:36:18 +0530

    fix global variable

--- RDF-Visualization/README.md
+++
@@ -1,8 +0,0 @@
-## How to run
-
-```bash
-
-docker build -t rdf-visualization-jayesh:latest .
-docker run -p 41022:5000 -e SSL_KEY=certs/key.key -e SSL_CERT=certs/cert.cert -t rdf-visualization-jayesh:latest
-
-```
--- RDF-Visualization/app.py
+++ RDF-Visualization/app.py
@@ -2,69 +2,44 @@ from flask import *
 import os 
 from rdflib.extras.external_graph_libs import rdflib_to_networkx_graph
 from pyvis.network import Network 
-from SPARQLWrapper import SPARQLWrapper, JSON , XML, N3, RDF, CSV, TSV, RDFXML 
+from SPARQLWrapper import SPARQLWrapper, JSON , XML
 import requests 
 app = Flask('testapp')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/updategraphvalue')
-def generate_graph_id():
-    global graph_id   
-    get_question = requests.get("https://dbpediachatbot.herokuapp.com/question").content
-    question = get_question.decode("utf-8")  
-    get_graph_id = requests.post('https://webengineering.ins.hs-anhalt.de:43740/startquestionansweringwithtextquestion',
-            params={
-                    "question": question,
-                    "componentlist[]": ["NED-DBpediaSpotlight",
-                    "QueryBuilderSimpleRealNameOfSuperHero",
-                    "SparqlExecuter",
-                    "OpenTapiocaNED",
-                    "BirthDataQueryBuilder",
-                    "WikidataQueryExecuter"]
-    })
-    res_json = get_graph_id.json()   
-    graph_id = res_json['inGraph'] 
-    return graph_id
-
-
-@app.route('/showgraph')
-def show_graph(): 
+@app.route('/visualize/<name>')
+def show_graph(name):
     get_graph_id = requests.get("https://dbpediachatbot.herokuapp.com/graphId").content
-    graph_id = get_question.decode("utf-8") 
-    print(graph_id)
-    sparql_database = SPARQLWrapper("https://webengineering.ins.hs-anhalt.de:40159/qanary/query")
-    sparql_database.setCredentials("admin", "admin")
-    sparql_query = """ 
-    DESCRIBE *
-    FROM <""" + graph_id  + """>
-    WHERE {
-            ?s a qa:AnnotationOfAnswerSPARQL.
-            ?s oa:hasBody ?sparqlQueryOnDBpedia .
-            ?s oa:annotatedBy ?annotatingService .
-            ?s oa:annotatedAt ?time .
-        }
-        """  
-    sparql_database.setQuery(sparql_query)
-    sparql_database.setReturnFormat(XML)
-    sparql_database.setMethod("POST")
-    results = sparql_database.query().convert()  
-    networkx_graph = rdflib_to_networkx_graph(results) 
-    net = Network(height="750px", width="100%")
-    net.from_nx(networkx_graph)
-    net.show_buttons(filter_=['physics'])
-    net.force_atlas_2based(gravity=-90,spring_length = 175, central_gravity = 0) 
-    save = net.show("templates/index.html")
-    return render_template('index.html')
-
-
-@app.route('/visualize/<name>')  
-def user(name):  
-    global graph_id
+    graph_id = get_graph_id.decode("utf-8")  
     if name == graph_id:
-        return redirect(url_for('show_graph')) 
+        sparql_database = SPARQLWrapper("https://webengineering.ins.hs-anhalt.de:40159/qanary/query")
+        sparql_database.setCredentials("admin", "admin")
+        sparql_query = """ 
+        DESCRIBE *
+        FROM <""" + graph_id  + """>
+        WHERE {
+                ?s a qa:AnnotationOfAnswerSPARQL.
+                ?s oa:hasBody ?sparqlQueryOnDBpedia .
+                ?s oa:annotatedBy ?annotatingService .
+                ?s oa:annotatedAt ?time .
+            }
+            """   
+        sparql_database.setQuery(sparql_query)
+        sparql_database.setReturnFormat(XML)
+        sparql_database.setMethod("POST")
+        results = sparql_database.query().convert()  
+        networkx_graph = rdflib_to_networkx_graph(results) 
+        print("networkx Graph loaded successfully with length {}".format(len(networkx_graph))) 
+        net = Network(height="750px", width="100%")
+        net.from_nx(networkx_graph)
+        net.show_buttons(filter_=['physics'])
+        net.force_atlas_2based(gravity=-90,spring_length = 175, central_gravity = 0)
+        save = net.show("templates/index.html")
+        return render_template('index.html')
     else:
         error_page = "Visualization Page not found Try Again... graph_id invalid"
         return error_page
+
 
 @app.route('/visualize/example')
 def html_page():  
@@ -79,7 +54,7 @@ def html_page():
                     "WikidataQueryExecuter"]
     })
     res_json = get_graph_id.json()   
-    graph_id_test = res_json['inGraph'] 
+    graph_id_test = res_json['inGraph']  
     sparql_database = SPARQLWrapper("https://webengineering.ins.hs-anhalt.de:40159/qanary/query")
     sparql_database.setCredentials("admin", "admin")
     sparql_query = """ 
@@ -91,12 +66,12 @@ def html_page():
             ?s oa:annotatedBy ?annotatingService .
             ?s oa:annotatedAt ?time .
         }
-        """  
+        """   
     sparql_database.setQuery(sparql_query)
     sparql_database.setReturnFormat(XML)
     sparql_database.setMethod("POST")
     results = sparql_database.query().convert()  
-    networkx_graph = rdflib_to_networkx_graph(results) 
+    networkx_graph = rdflib_to_networkx_graph(results)  
     net = Network(height="750px", width="100%")
     net.from_nx(networkx_graph)
     net.show_buttons(filter_=['physics'])
